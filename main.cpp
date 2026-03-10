1738ef5eb2b68f09b98cdd4eb58c425533e80536
Seetaar <setaloelizaveta@gmail.com>
2026-03-11 02:38:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,14 @@
 int main() {
     Logger::info("Laboratory Work #1");
 
-    // 1. Инициализация параметров 
     float k = 0.8f;
     float b = 10.0f;
 
-    // 2. Генерация данных
     Logger::info("Generating dataset...");
     auto dataset = DataProcessor<float>::generateDataset(200, k, b);
 
-    // 3. Демонстрация работы с матрицами 
     MatrixF transform(2, 2);
-    transform(0,0) = 1.0f; transform(0,1) = 0.1f; // сдвиг
+    transform(0,0) = 1.0f; transform(0,1) = 0.1f; 
     transform(1,0) = 0.0f; transform(1,1) = 1.0f;
 
     auto [oldX, oldY] = std::make_pair(dataset[0].x, dataset[0].y);
@@ -26,11 +23,9 @@ int main() {
     Logger::logValue("Original X", oldX);
     Logger::logValue("Transformed X", newX);
 
-    // 4. Расчет итоговой метрики Loss 
     float totalLoss = Metrics<float>::totalAbsoluteError(dataset, k, b);
     Logger::logValue("Final Absolute Loss", totalLoss);
 
-    // 5. Сохранение для визуализации
     if (DataProcessor<float>::saveToCSV(dataset, "points.csv")) {
         Logger::info("Success: 'points.csv' generated for Python visualization.");
     }
